cefa52d403f0f2c646de60fb122fb59b9a34f4a2
Soslan Aldatov <me@supporteam.ru>
2015-08-14 18:06:30 +0500

    username generation addede

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 #!flask/bin/python
-from flask import Flask
+import string
+import random
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return "Hello, world!"
+@app.route('/accounts/create', methods=['POST'])
+def get_answer():
+	username = 'user' + ''.join(random.choice(string.digits) for _ in range(5))
+	return username;
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True, host='0.0.0.0')
